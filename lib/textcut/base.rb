6b3ba5efdf7f3395146aaa21674313d5d4206d33
Tyralion <piliaiev@gmail.com>
2011-11-11 15:06:34 +0600

    Добавлены примеры в readme.

--- lib/textcut/base.rb
+++ lib/textcut/base.rb
@@ -3,16 +3,6 @@ module Textcut
 
   module Base
 
-    #
-    # ev = Event.last
-    # ev.update_attributes!({ :content => "<p>test 1<span class='textcut'></span> <i>lol</i></p>" })
-    # Event.last.update_attributes!({ :content => "<p>test 2<span class='textcut'>ReadMe</span> <i>lol</i></p>" })
-    # Event.last.update_attributes!({ :content => "<p>test 3 <i>lol</i></p>" })
-    #
-    # ev.content
-    # ev.content_cut
-    # Event.last.description
-
     def self.included(base)
       base.extend(SingletonMethods)
     end # self.included
